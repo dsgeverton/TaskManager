949a4b239ceddeb64cf1cce60e9cf1416a4e5554
Everton <dsgeverton@gmail.com>
2018-11-09 22:37:37 -0200

    wip 0.1

--- app/src/main/java/br/com/everoot/tarefasescolar/TesteFirebaseActivity.java
+++ app/src/main/java/br/com/everoot/tarefasescolar/TesteFirebaseActivity.java
@@ -2,12 +2,33 @@ package br.com.everoot.tarefasescolar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TesteFirebaseActivity extends AppCompatActivity {
+
+    private ViewHolder viewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teste_firebase);
+
+        viewHolder.edtNomeTurma = findViewById(R.id.edtNomeTurma);
+        viewHolder.edtNumeroTurma = findViewById(R.id.edtNumeroTurma);
+        viewHolder.btnSalvar = findViewById(R.id.btnSalvarTurma);
+
+        viewHolder.btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add in firebase
+            }
+        });
+    }
+
+    public class ViewHolder{
+        EditText edtNomeTurma, edtNumeroTurma;
+        Button btnSalvar;
     }
 }
