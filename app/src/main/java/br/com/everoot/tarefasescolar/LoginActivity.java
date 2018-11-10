b72cdd36c7a10fbdebdc1c3b5b5ba4410b8ba29e
Everton <dsgeverton@gmail.com>
2018-11-10 09:09:28 -0200

    Login implementation codes

--- app/src/main/java/br/com/everoot/tarefasescolar/LoginActivity.java
+++ app/src/main/java/br/com/everoot/tarefasescolar/LoginActivity.java
@@ -1,13 +1,111 @@
 package br.com.everoot.tarefasescolar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final String TAG = "TESTE";
+    private static final int RC_SIGN_IN = 9001;
+    private ViewHolder mviewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mviewHolder.sign_in = findViewById(R.id.sign_in_button);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+
     }
+
+    private void updateUI(GoogleSignInAccount account) {
+
+        if (account != null){
+            Log.i("GOOGLE SIGN IN ID:", account.getId());
+            Log.i("GOOGLE SIGN IN NAME:", account.getDisplayName());
+            Log.i("GOOGLE SIGN IN EMAIL:", account.getEmail());
+//            Log.i("GOOGLE SIGN IN PHOTO:", account.getPhotoUrl().toString());
+//            Intent intent = new Intent(this, IngressActivity.class);
+//            startActivity(intent);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.sign_in_button){
+            signIn();
+        }
+        else{
+            // other thing
+        }
+    }
+
+    public static class ViewHolder{
+        SignInButton sign_in;
+    }
+
 }
