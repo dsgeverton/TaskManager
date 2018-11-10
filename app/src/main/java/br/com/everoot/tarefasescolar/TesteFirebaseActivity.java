06076658db3cb770b80d98fd08edb61bd9e777db
Everton <dsgeverton@gmail.com>
2018-11-09 22:29:53 -0200

    Add firebase test activity in branch "teste-firebase"

---
+++ app/src/main/java/br/com/everoot/tarefasescolar/TesteFirebaseActivity.java
@@ -0,0 +1,13 @@
+package br.com.everoot.tarefasescolar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TesteFirebaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste_firebase);
+    }
+}
