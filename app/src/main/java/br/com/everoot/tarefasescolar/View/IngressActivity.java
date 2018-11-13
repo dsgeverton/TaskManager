a502c30dee71a26bbbce68f21730c89e185f4807
Everton <dsgeverton@gmail.com>
2018-11-13 03:27:18 -0200

    Add dialog in ingress activity

--- app/src/main/java/br/com/everoot/tarefasescolar/View/IngressActivity.java
+++ app/src/main/java/br/com/everoot/tarefasescolar/View/IngressActivity.java
@@ -1,13 +1,10 @@
 package br.com.everoot.tarefasescolar.View;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,6 +13,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
+import com.github.javiersantos.materialstyleddialogs.enums.Style;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,7 +26,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import br.com.everoot.tarefasescolar.Model.Turma;
-import br.com.everoot.tarefasescolar.Model.Usuario;
 import br.com.everoot.tarefasescolar.R;
 
 public class IngressActivity extends AppCompatActivity implements View.OnClickListener{
@@ -119,9 +119,40 @@ public class IngressActivity extends AppCompatActivity implements View.OnClickLi
                     Turma turma = dataSnapshot.child(turmaID).getValue(Turma.class);
                     if (turma==null){
                         Toast.makeText(IngressActivity.this, "Turma não encontrada", Toast.LENGTH_SHORT).show();
+                        new MaterialStyledDialog.Builder(IngressActivity.this)
+                                .setTitle("Turma não encontrada!\n \uD83D\uDE1E")
+                                .setStyle(Style.HEADER_WITH_TITLE)
+                                .setDescription("Talvez você tenha errado o ID... \uD83D\uDE1E")
+                                .setPositiveText("OK")
+                                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                    @Override
+                                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                        Log.d("MaterialStyledDialogs", "Do something!");
+                                    }})
+                                .show();
+
                     } else {
                         Toast.makeText(IngressActivity.this, "Turma encontrada", Toast.LENGTH_SHORT).show();
                         Log.d("TAG", "Value is: " + turma.getNome());
+                        new MaterialStyledDialog.Builder(IngressActivity.this)
+                                .setTitle("Turma encontrada!")
+                                .setDescription("A turma que você está prestes a ingressar é: \n\n"+ turma.toString())
+                                .setIcon(R.drawable.me)
+                                .setHeaderDrawable(R.drawable.help)
+                                .withDarkerOverlay(true)
+                                .setPositiveText("OK")
+                                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                    @Override
+                                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                        Log.d("MaterialStyledDialogs", "Do something!");
+                                    }})
+                                .setNegativeText("Calcelar")
+                                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                                    @Override
+                                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                        Log.d("MaterialStyledDialogs", "Do something!");
+                                    }})
+                                .show();
 
                     }
                 }
